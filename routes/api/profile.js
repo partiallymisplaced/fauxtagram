1894b7f845066b843d35bd1bafc7147cfbf341a8
Eury <32747825+AFMR@users.noreply.github.com>
2018-10-13 10:08:54 -0700

    Fixed bugs on profiles.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -51,43 +51,4 @@ router.post('/', passport.authenticate('jwt', {session: false}),
       });
   })
 
-// @route   POST api/profile/follow
-// @desc    Create or edit user profile
-// @access  Private
-router.post('/follow', passport.authenticate('jwt', {session: false}),
-  (req, res) => {
-    let errors = {};
-    
-    if (!req.body.userIdToFollow) {
-        errors.noprofile = 'A user to follow must be specified.';
-        return res.status(400).json(errors);
-    }
-
-    // Add to current user's following array
-    Profile.findOne({user: req.user.id})
-      .then(profile => {
-        if (profile) {
-          profile.following
-          .insert({userId: req.body.userIdToFollow})
-          .then(profile => res.json(profile));
-        } else {
-            errors.noprofile = 'Profile not found.';
-            return res.status(404).json(errors);
-        }
-      });
-
-      // Add to followers array of user being followed.
-      Profile.findOne({user: req.body.userIdToFollow})
-        .then(profile => {
-          if (profile) {
-            profile.followers
-            .insert({userId: req.user.id});
-          } else {
-              errors.noprofile = 'Profile not found.';
-              return res.status(404).json(errors);
-          }
-        });
-  }
-)
-
 module.exports = router;
