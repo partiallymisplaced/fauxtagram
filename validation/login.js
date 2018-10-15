8b4eeaaccf750fe5c7c13af80023420eabced144
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2018-10-15 15:54:26 -0700

    Adds validation for signup and login (#4)
    * Removes a stray console.log
    
    * Adds validation for signup, login
    
    * Fixes missing export for empty normalization
    
    * Fixes error with login import (by uncommenting)
    
    * Fixes password validation min & max, comments out mobile validation

---
+++ validation/login.js
@@ -0,0 +1,17 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.username = !isEmpty(data.username) ? data.username : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Username cannot be empty";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password cannot be empty"
+  }
+  return { errors, isValid: isEmpty(errors) }
+}
